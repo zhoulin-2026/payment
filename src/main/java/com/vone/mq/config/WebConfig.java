31e29d5ac1035e8af50074d4b9e50c7993dddf29
zql <Mfk@88231188>
2026-04-29 19:31:17 +0800

    跨域

--- src/main/java/com/vone/mq/config/WebConfig.java
+++ src/main/java/com/vone/mq/config/WebConfig.java
@@ -10,10 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .exposedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(3600);
     }
